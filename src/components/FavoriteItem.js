23479d7921aff86c76ad7c33af3deb0837972d30
Nicolás Gómez <ngomez@hey.com>
2022-10-25 21:41:19 -0300

    feat: add bootstrap

--- src/components/FavoriteItem.js
+++ src/components/FavoriteItem.js
@@ -1,9 +1,11 @@
 export default function FavoriteItem({ image, intruction, description }) {
   return (
-    <div>
-      <img src={image} alt="" />
-      <p className="bold">{intruction}</p>
-      <p>{description}</p>
+    <div className="card">
+      <img className="card-img-top" src={image} alt="" />
+      <div className="card-body">
+        <h5 className="card-title">{intruction}</h5>
+        <p className="card-text">{description}</p>
+      </div>
     </div>
   )
 }
